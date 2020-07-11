cb90611faf7cf82d90ab7ccfba968d29656a619a
김준호 <kimjunho@wemakeprice.com>
2020-07-12 02:38:54 +0900

    - 카테고리 Entity 불필요 Field Ignore 처리

--- src/main/java/com/amorepacific/inmemorycache/domain/CacheCategory.java
+++ src/main/java/com/amorepacific/inmemorycache/domain/CacheCategory.java
@@ -1,15 +1,19 @@
 package com.amorepacific.inmemorycache.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class CacheCategory {
     // 카테고리 번호
+    @JsonIgnore
     private Long categoryNo;
     // 카테고리명
     private String categoryName;
     // 상위 카테고리
+    @JsonIgnore
     private String parentNo;
     // 카테고리 뎁스
+    @JsonIgnore
     private String depth;
 }
